fcfd49fa94560e2503f6a6b585ba1bb794933aaa
cprogress-dnldrymnd <donald@cprogress.co.uk>
2024-04-30 14:17:21 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -72,7 +72,7 @@ add_action('widgets_init', 'wpdocs_theme_slug_widgets_init');
 
 
 remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart');
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 99);
 
 function action_woocommerce_after_shop_loop_item()
 {
